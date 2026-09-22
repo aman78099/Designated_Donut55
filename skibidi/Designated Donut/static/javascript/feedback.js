f738fa652e46b35fc0c42d96ab6963a6ed830a78
aman78099 <amanrawat@riccarton.school.nz>
2026-09-22 21:18:46 +1200

    Delete skibidi directory

--- skibidi/Designated Donut/static/javascript/feedback.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementById("feedbackForm");
-    const button = document.getElementById("Submit");
-
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        button.textContent = "Thank you!";
-    });
-});
